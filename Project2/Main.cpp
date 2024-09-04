54a65f57a0f4a89a585f77b8bffd59395db25486
oleh-tft <tiftelalbion@gmail.com>
2024-09-04 11:40:23 +0300

    s

--- Project2/Main.cpp
+++ Project2/Main.cpp
@@ -2,12 +2,41 @@
 
 using namespace std;
 
-void Meow() 
+class Point
 {
-	cout << "Meow" << endl;
-}
+private:
+	int x, y;
+public:
+	void Print()
+	{
+		cout << "X: " << x << "\tY: " << y << endl;
+	}
+
+	void init(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+
+	int GetX() {
+		return x;
+	}
+
+	int GetY() {
+		return y;
+	}
+};
 
 int main()
 {
-	Meow();
+	Point a;
+	a.init(1, 2);
+	a.Print();
+
+	Point b;
+	b.init(10, 20);
+	b.Print();
+
+	cout << a.GetX() << endl;
+	cout << b.GetX() << endl;
 }
